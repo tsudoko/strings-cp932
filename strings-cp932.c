b56f2b77c36f6adc9fdf71b195121b0137cf7174
flan <flan@flande.re>
2019-09-16 17:17:25 +0200

    Initial commit

---
+++ strings-cp932.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// needs to be at least 2
+#define BUFLEN 3
+
+int
+main(int argc, char **argv)
+{
+	if(argc < 2) {
+		fprintf(stderr, "usage: %s file\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	FILE *f = fopen(argv[1], "rb");
+	unsigned char buf[BUFLEN], *c = buf;
+	size_t n, toread = BUFLEN;
+	_Bool waschar = 0;
+	while(!feof(f)) {
+		if((n = fread(c, 1, toread, f)) != toread && ferror(f)) {
+				fprintf(stderr, "read error at %ld\n", ftell(f));
+				return EXIT_FAILURE;
+		}
+		fprintf(stderr, "read %d\n", n);
+		toread = BUFLEN;
+
+		while(c-buf < BUFLEN) {
+			if((*c < 0x7f && *c >= 0x20)) {
+				if(!waschar) { putchar('\n'); waschar = 1; }
+				putchar(*c++);
+				continue;
+			}
+
+			if(
+				(*c >= 0x81 && *c <= 0x9f) ||
+				(*c >= 0xe0 && *c <= 0xef) || // sjis
+				(*c >= 0xfa && *c <= 0xfc) // cp932 extensions
+			) {
+				if(BUFLEN-(c-buf) < 2) {
+					memmove(buf, c, BUFLEN-(c-buf));
+					toread = c-buf;
+					break;
+				}
+
+				if(*c % 2 == 0 &&
+					(c[1] >= 0x9f && c[1] <= 0xfc) ||
+					(c[1] >= 0x40 && c[1] <= 0x9e && c[1] != 0x7f)
+				) {
+					if(!waschar) { putchar('\n'); waschar = 1; }
+					putchar(*c++);
+					putchar(*c++);
+					continue;
+				}
+			}
+
+			waschar = 0;
+			c++;
+		}
+		c = buf;
+	}
+	putchar('\n');
+
+	return EXIT_SUCCESS;
+}
